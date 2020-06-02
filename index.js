13e0d456f3c05d1000433c68d6b062fb004d8333
Muhammad Ismail <47635180+ismardoi2001@users.noreply.github.com>
2020-06-02 12:28:30 +0500

    added js page

---
+++ index.js
@@ -0,0 +1,8 @@
+
+var num = parseInt(prompt("Please enter number"));
+
+for (var i=1;i<=10; i++){
+
+   document.write(num + " * "+i+" = "+(num * i))
+  
+}
---
+++ loopbreak.js
@@ -0,0 +1,8 @@
+
+for (var i = 0; i < 8; i++){
+    if(i == 4) {
+        break
+    }
+    document.write("I = "+i);
+}
+
